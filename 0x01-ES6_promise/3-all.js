9e2672b52ecf254ad5033c10fadd531f3183bdbe
Parsitau-michael <parsitaumichael@gmail.com>
2024-10-22 12:50:18 +0000

    Some few corrections

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResonse, userResponse]) => {
       console.log(`${photoResonse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
 
-   try {
-     const result = mathFunction();
-     queue.push(result);
-   } catch {
-     queue.push(error.message);
-   }
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  }
 
   queue.push('Guardrail was processed');
   return queue;

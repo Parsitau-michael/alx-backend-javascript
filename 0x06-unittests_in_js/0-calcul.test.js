f2ff5a02a5053a04c9d57770e4ca4f22e7abd8f1
Parsitau-michael <parsitaumichael@gmail.com>
2025-01-21 08:47:19 +0000

    Set up package and ran basic mocha tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,9 +2,39 @@ const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
 describe('calculateNumber', function() {
-  it('Should sum two floating points', function() {
+  it('testing round-off when both arguments are already integers', function() {
+    const actualRes = calculateNumber(1, 3);
+    const expectedRes = 4;
+    assert.equal(actualRes, expectedRes);
+  });
+
+  it('testing round-off when both arguments are floating-point numbers', function() {
     const actualRes = calculateNumber(1.1, 3.6);
     const expectedRes = 5;
+    assert.equal(actualRes, expectedRes);
+  });
+
+  it('testing round-off when one argument is floating-point the other an integer', function() {
+    const actualRes = calculateNumber(1.1, 3);
+    const expectedRes = 4;
+    assert.equal(actualRes, expectedRes);
+  });
+
+  it('testing round-off with negative arguments, a floating-point the other an integer', function() {
+    const actualRes = calculateNumber(-1.1, -3);
+    const expectedRes = -4;
+    assert.equal(actualRes, expectedRes);
+  });
+
+  it('testing round-off with arguments with very large numbers', function() {
+    const actualRes = calculateNumber(0.1, 0.9);
+    const expectedRes = 1;
+    assert.equal(actualRes, expectedRes);
+  });
+
+  it('testing round-off with arguments close to .5', function() {
+    const actualRes = calculateNumber(1.4, 2.5);
+    const expectedRes = 4;
     assert.equal(actualRes, expectedRes);
   });
 });

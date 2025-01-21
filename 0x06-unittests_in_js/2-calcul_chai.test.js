675c2930be1dc858c37648de95ed253b77ea8a3e
Parsitau-michael <parsitaumichael@gmail.com>
2025-01-21 09:55:37 +0000

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,29 @@
+const calculateNumber = require('./1-calcul.js');
+const expect = require('chai').expect;
+
+describe('calculateNumber', function() {
+  it('test type = SUM', function() {
+    const actualOut = calculateNumber('SUM', 1.4, 0.9);
+    const expectedOut = 2;
+    expect(actualOut).to.equal(expectedOut);
+  });
+
+  it('test type = SUBTRACT', function() {
+    const actualOut = calculateNumber('SUBTRACT', -1.4, 0.9);
+    const expectedOut = -2;
+    expect(actualOut).to.equal(expectedOut);
+  });
+
+  it('test type = DIVIDE', function() {
+    const actualOut = calculateNumber('DIVIDE', 10.4, 1.9);
+    const expectedOut = 5;
+    expect(actualOut).to.equal(expectedOut);
+  });
+
+  it('test type = DIVIDE with rounded b = 0', function() {
+    const actualOut = calculateNumber('DIVIDE', 10.4, 0.4);
+    const expectedOut = 'Error';
+    expect(actualOut).to.be.a('string');
+    expect(actualOut).to.equal(expectedOut);
+  });
+});

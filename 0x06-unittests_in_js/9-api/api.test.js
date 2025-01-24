973296e29f072039cf8457da96150e6b6830559b
Parsitau-michael <parsitaumichael@gmail.com>
2025-01-24 07:11:18 +0000

    Included a test for the root page

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,17 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Basic Integration testing', function() {
+  it('Should test the index page', function(done) {
+    request('http://localhost:7865/', (error, response, body) => {
+
+      expect(error).to.be.null;
+      expect(response.statusCode).to.equal(200);
+      expect(response.body).to.equal('Welcome to the payment system');
+
+      done();
+    });
+  });
+
   it('Should test the cart page when id is a number', function(done) {
     request('http://localhost:7865/cart/12', (error, response, body) => {
 
@@ -15,6 +26,7 @@ describe('Basic Integration testing', function() {
 
   it('Should test the cart page when id is not a number', function(done) {
     request('http://localhost:7865/cart/hello', (error, response, body) => {
+
       expect(error).to.be.null;
       expect(response.statusCode).to.equal(404);
 

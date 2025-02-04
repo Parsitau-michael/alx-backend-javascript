3ffdeab46b114e9571a34266b58ea2d4cd416116
Parsitau-michael <parsitaumichael@gmail.com>
2025-02-04 18:26:36 +0000

    Iterate through object using for...of

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const names = [];
+  for (const item of reportWithIterator) {
+    names.push(item);
+  }
+  return names.join(' | ');
+}

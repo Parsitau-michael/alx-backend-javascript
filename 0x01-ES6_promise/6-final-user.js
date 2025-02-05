cc62d7d5c37dba3474abba82a15adbeb379a0234
Parsitau-michael <parsitaumichael@gmail.com>
2025-02-05 17:14:14 +0000

    Debbuging task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => (
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }))
-  ));
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(([signUpUserResp, uploadPhotoResp]) => [
+      { status: signUpUserResp.status, value: signUpUserResp },
+      { status: uploadPhotoResp.status, value: uploadPhotoResp },
+    ]);
 }
+
+module.exports = handleProfileSignup;

17989b9af6ad710626f9a63cda8a6dc2677d9740
Parsitau-michael <parsitaumichael@gmail.com>
2025-02-11 18:40:52 +0000

    Corrected some error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,11 @@
 export default function cleanSet(set, startString) {
+  if (startString.length === 0) {
+    return '';
+  }
+
   const array = [];
   set.forEach((item) => {
-    if (item.trim().includes(startString) && startString.length !== 0) {
+    if (item.trim().includes(startString)) {
       array.push(item.slice(startString.length));
     }
   });

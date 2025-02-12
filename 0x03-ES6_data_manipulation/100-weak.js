b1b83a881a73427e0ad069fbcb71711af0b555bc
Parsitau-michael <parsitaumichael@gmail.com>
2025-02-12 07:45:32 +0000

    Last modification on WeakMaps

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,9 @@ export function queryAPI(endpointArg) {
   // Get the current count for the endpoint, defaulting to 0 if not found
   let count = weakMap.get(endpointArg) || 0;
 
-  if (count >= 5) throw new Error('Endpoint load is high');
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 
   count += 1;
   weakMap.set(endpointArg, count);

6117a8742f94a41ec5ca704d4cb16a3cdaed975c
Parsitau-michael <parsitaumichael@gmail.com>
2025-06-23 14:10:47 +0000

    Improved code making it less verbose

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,9 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 async function asyncUploadUser() {
   try {
-    const uploadPhotoResponse = await (uploadPhoto());
-    const createUserResponse = await (createUser());
-    return { photo: uploadPhotoResponse, user: createUserResponse };
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
   } catch (error) {
     return { photo: null, user: null };
   }

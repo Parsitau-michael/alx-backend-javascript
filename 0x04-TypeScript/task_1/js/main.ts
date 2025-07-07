80f90f71dab22f97aa198dd74bb1d046fcca0faf
Parsitau-michael <parsitaumichael@gmail.com>
2025-07-07 08:47:53 +0000

    Implemented interfaces for both a class and its constructor

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,5 @@
+// Task 1
+
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -17,6 +19,8 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
+// Task 2
+
 interface Directors extends Teacher {
   numberOfReports: number;
 }
@@ -30,3 +34,50 @@ const director1: Directors = {
 };
 console.log(director1);
 
+// Task 3
+
+interface printTeacherFunction {
+ (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function(firstName, lastName) {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+console.log(printTeacher("John", "Doe"));
+
+// Task 4
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return `Currently working`;
+  }
+
+  displayName() {
+    return `${this.firstName}`;
+  }
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+  }
+
+const std1 = new StudentClass('Mike', 'Lucky');
+console.log(std1.workOnHomework());
+const std2 = new StudentClass('Mike');
+console.log(std2.displayName());
+const std3: StudentConstructor = new StudentClass('Mike');
+console.log(std3.displayName());
